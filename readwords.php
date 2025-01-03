58c28e9f8c8b10818ca2cc11ab036fd593fff57d
Benjamin Carlyle <benjamincarlyle@soundadvice.id.au>
2025-01-03 14:04:57 +1000

    Read dictionary words with identifiers

--- readwords.php
+++ readwords.php
@@ -6,6 +6,15 @@ $DOM->loadHTML(source: $dictionarywords);
 $finder = new DomXPath($DOM);
 $entries = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' entry ')]");
 
-for ($i = 0; $i < $entries->length; $i++)
-echo $entries->item($i)->nodeValue . "\r\n";
+for ($ii = 0; $ii < $entries->length; $ii++) {
+    $entry = $entries->item($ii);
+    // echo $entry->nodeValue . "\r\n";
+    echo $id . "\r\n";
+    $id = $entry->getAttribute("id");   
+    $gyy = $finder->query(
+        "./div/*[contains(concat(' ', normalize-space(@class), ' '),
+        ' hw ')]",
+        $entry);
+    echo $gyy->item(0)->nodeValue . "\r\n";
+}
 ?>
